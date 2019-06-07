2ec7eacad43afa02b27d4af44843d8da5f99b8ff
Justin Liles <Justin.Liles@Live.com>
2019-06-06 23:36:48 -0500

    Testing MessageForPlayer in main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 #include "debug.hpp"
+#include "console.hpp"
 
 int main( int argc, const char* argv[] )
 {
 	Log( "Hello RPG" );
+
+	std::string possibleChoices[] = {"1", "2", "3"};
+	MessageForPlayer("Testing Qestion", possibleChoices);
 }
