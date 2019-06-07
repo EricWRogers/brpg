a9ad8d053eedcd93c37703d8c7f6d198c632bff0
Justin Liles <Justin.Liles@Live.com>
2019-06-06 23:35:42 -0500

    Added MessageForPlayer funchion to console.cpp.

---
+++ src/console.cpp
@@ -0,0 +1,22 @@
+#include "console.hpp"
+#include "debug.hpp"
+#include <iostream>
+
+void MessageForPlayer(std::string messageToDisplay, std::string possibleChoices[])
+{
+    bool loopControl = true;
+    std::string response = "";
+
+    while(loopControl)
+    {
+        std::cout << "" << std::endl;
+        std::cout << messageToDisplay << std::endl;
+
+        for(int i = 0; i < std::size(*possibleChoices); i++)
+        {
+            std::cout << i + ": " + possibleChoices[i] << std::endl;
+        }
+
+        std::cin >> response;
+    }
+}
---
+++ src/console.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+void MessageForPlayer(std::string messageToDisplay, std::string possibleChoices[]);
